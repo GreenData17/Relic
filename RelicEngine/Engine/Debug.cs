4b8c7155aac9f0410883c615adbdd8e97b6f2075
GreenData17 <contact@greendata.dev>
2022-11-28 15:34:34 +0100

    Added a custom Log option

--- RelicEngine/Engine/Debug.cs
+++ RelicEngine/Engine/Debug.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,16 +11,22 @@ namespace Relic.Engine
     {
         public enum LogType
         {
-            Log, Warning, Error, Engine
+            Log, Warning, Error, Engine, Custom
         }
+
+        private static Vector4 logColor = new Vector4(1, 1, 1, 1);
+        private static Vector4 warningColor = new Vector4(1, 0, 0, 1);
+        private static Vector4 errorColor = new Vector4(1, 1, 1, 1);
+        private static Vector4 engineColor = new Vector4(0, .5f, .8f, 1);
 
         public static List<Messages> logs { get{return _logs;} private set{_logs = value;}}
         private static List<Messages> _logs = new List<Messages>();
 
-        public static void Log(string msg) { SendToConsole(msg, "LOG   ", ConsoleColor.White); logs.Add(new Messages(msg, LogType.Log));}
-        public static void LogWarning(string msg) { SendToConsole(msg, "WARN  ", ConsoleColor.Yellow); logs.Add(new Messages(msg, LogType.Warning)); }
-        public static void LogError(string msg) { SendToConsole(msg, "ERROR ", ConsoleColor.Red); logs.Add(new Messages(msg, LogType.Error)); }
-        public static void LogEngine(string msg) { SendToConsole(msg, "ENGINE", ConsoleColor.Cyan); logs.Add(new Messages(msg, LogType.Engine)); }
+        public static void Log(string msg) { SendToConsole(msg, "LOG   ", ConsoleColor.White); logs.Add(new Messages(msg, LogType.Log, logColor));}
+        public static void LogWarning(string msg) { SendToConsole(msg, "WARN  ", ConsoleColor.Yellow); logs.Add(new Messages(msg, LogType.Warning, warningColor)); }
+        public static void LogError(string msg) { SendToConsole(msg, "ERROR ", ConsoleColor.Red); logs.Add(new Messages(msg, LogType.Error, errorColor)); }
+        public static void LogEngine(string msg) { SendToConsole(msg, "ENGINE", ConsoleColor.Cyan); logs.Add(new Messages(msg, LogType.Engine, engineColor)); }
+        public static void LogCustom(string msg, string prefix, Vector4 color) { SendToConsole(msg, prefix, ConsoleColor.Cyan); logs.Add(new Messages(msg, LogType.Custom, color, prefix)); }
 
         //====================
 
@@ -41,14 +48,18 @@ namespace Relic.Engine
         public class Messages
         {
             public string msg;
+            public string prefix;
             public LogType type;
+            public Vector4 color;
             public DateTime time;
 
-            public Messages(string msg, LogType type)
+            public Messages(string msg, LogType type, Vector4 color, string prefix = "")
             {
                 this.msg = msg;
                 this.type = type;
                 this.time = DateTime.UtcNow;
+                this.color = color;
+                this.prefix = prefix;
             }
         }
     }
