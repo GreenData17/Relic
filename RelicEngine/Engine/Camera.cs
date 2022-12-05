eda1238a62466cd3b7f5059e6100cacb88280a43
GreenData17 <contact@greendata.dev>
2022-12-05 01:35:25 +0100

    Adding Camera Component

---
+++ RelicEngine/Engine/Camera.cs
@@ -0,0 +1,29 @@
+﻿namespace Relic.Engine
+{
+    public class Camera : MonoBehaviour
+    {
+        private Vector2 oldPosition;
+
+        public override void Start()
+        {
+            oldPosition = new(gameObject.transform.position.X, gameObject.transform.position.Y);
+        }
+
+        public override void Update()
+        {
+            if(oldPosition == null) return;
+            if(oldPosition.X == gameObject.transform.position.X && oldPosition.Y == gameObject.transform.position.Y) return;
+
+            var camPos = Window.mainCam.position;
+            var objPos = gameObject.transform.position;
+
+            Debug.Log($"camPos X:{camPos.X}|Y:{camPos.Y}");
+            Debug.Log($"objPos X:{objPos.X}|Y:{objPos.Y}");
+
+            Window.mainCam.position = new Vector2(Window.instance.ClientSize.X / 2f + objPos.X, Window.instance.ClientSize.Y / 2f + objPos.Y);
+            //Debug.Log("Position changed!");
+
+            oldPosition = new(gameObject.transform.position.X, gameObject.transform.position.Y);
+        }
+    }
+}
--- RelicEngine/Engine/OrthographicCamera.cs
+++ RelicEngine/Engine/OrthographicCamera.cs
@@ -1,57 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using OpenTK.Mathematics;
-using OpenTK;
+﻿using OpenTK.Mathematics;
 using Relic.DataTypes;
 
 namespace Relic.Engine
 {
     public class OrthographicCamera
     {
-        private Vector2 _position = new Vector2(0);
         public Vector2 position
         {
             get => _position;
             set { _position = value; RecalculateViewMatrix(); }
         }
+        private Vector2 _position;
 
-
-        private float _rotation = 0f;
-        public float rotation
-        {
-            get => _rotation;
-            set { _rotation = value; RecalculateViewMatrix(); }
-        }
-
+        // Camera Specific Variables
         public Matrix4 view;
         public Matrix4 projection;
-
+        
         public BufferTexture bufferTexture;
+        
 
-        public OrthographicCamera(float left, float right, float bottom, float top)
+        public OrthographicCamera()
         {
-            position = new Vector2(Window.windowSize.X / 2f, Window.windowSize.Y / 2f);
-            //view = Matrix4.CreateTranslation(position.X, position.Y, -3.0f);
+            position = new(Window.instance.ClientSize.X / 2f, Window.instance.ClientSize.Y / 2f);
             RecalculateViewMatrix();
-            RecalculateProjectionMatrix(left, right, bottom, top);
+            RecalculateProjectionMatrix();
         }
 
         public void RecalculateViewMatrix()
         {
-            view = Matrix4.CreateTranslation(position.X, position.Y, -3.0f);
+           view = Matrix4.CreateTranslation(position.X, position.Y, -3.0f);
         }
 
-        public void RecalculateProjectionMatrix(float left, float right, float bottom, float top)
+        public void RecalculateProjectionMatrix()
         {
-            projection = Matrix4.CreateOrthographicOffCenter(left, right, bottom, top, 0.1f, 1000.0f);
+            projection = Matrix4.CreateOrthographicOffCenter(0, Window.instance.ClientSize.X, 0, Window.instance.ClientSize.Y, 0.1f, 1000.0f);
         }
 
-        public void UpdateBuffer()
-        {
-            bufferTexture.Update();
-        }
+        public void CreateBufferTexture() => bufferTexture = new BufferTexture();
     }
 }
