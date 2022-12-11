74de377474b660ba80481b1d1c7b6fc26e443a4e
GreenData17 <contact@greendata.dev>
2022-12-11 03:05:22 +0100

    Added Scene Class

---
+++ RelicEngine/Engine/Scene.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Relic.Engine
+{
+    public class Scene
+    {
+        public string name = "New Scene";
+        public GameObject mainCamera;
+        public List<GameObject> gameObjects = new List<GameObject>();
+
+        public Scene()
+        {
+            var camObj = Instantiate(new GameObject());
+            camObj.name = "Main Camera";
+            var camComp = new Camera();
+            camComp.mainCamera = true;
+            camObj.AddComponent(camComp);
+            mainCamera = camObj;
+            camObj = null;
+
+
+
+            camObj = Instantiate(new GameObject());
+            camObj.name = "Main Camera2";
+            camComp = new Camera();
+            camObj.AddComponent(camComp);
+            camObj = null;
+        }
+
+        public void Update()
+        {
+            foreach (var gameObject in gameObjects)
+            {
+                if (!gameObject.enabled) return;
+
+                gameObject.Update();
+            }
+        }
+
+        public GameObject Instantiate(GameObject gameObject)
+        {
+            gameObjects.Add(gameObject);
+            Window.loadedGameobjects += 1;
+            return gameObject;
+        }
+
+        public void DeleteGameObject(GameObject gameObject)
+        {
+            gameObjects.Remove(gameObject);
+            Window.loadedGameobjects -= 1;
+        }
+    }
+}
