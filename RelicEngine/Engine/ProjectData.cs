ec79845d9a5a4c21b35193bb7672ff902c7093bd
GreenData17 <e.eggenberger@quickline.com>
2023-02-02 23:12:33 +0100

    preparing ProjectData Saving

---
+++ RelicEngine/Engine/ProjectData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Relic.Engine
+{
+
+    // TODO: save and load Data
+    public class ProjectData
+    {
+        // TODO: OnStart open this scene!
+        public string LastOpenScene = "Assets\\Scenes\\New Scene.scene";
+    }
+}
